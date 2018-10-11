71cb68b135cad21398d1daca1d8c5dc27f5a118f
anik1606 <anik1606@gmail.com>
2018-10-11 18:03:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,13 @@ def oxford_comma(array)
     return array.join()
   elsif array.size == 2 
   return array.join(" and ")
+  elsif array.size >= 3
+  array = array.insert(-2, "and")
+  new_arr = []
+  array.each do |ele|
+    new_ele = ele + ","
+    new_arr.push(new_ele)
+  
   end
 
 end
