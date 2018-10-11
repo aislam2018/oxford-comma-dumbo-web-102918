a65942de6d45b81a511aacb69b28bdbb9a3f9564
anik1606 <anik1606@gmail.com>
2018-10-11 18:04:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,14 +2,15 @@ def oxford_comma(array)
   if array.size == 1 
     return array.join()
   elsif array.size == 2 
-  return array.join(" and ")
+    return array.join(" and ")
   elsif array.size >= 3
-  array = array.insert(-2, "and")
-  new_arr = []
-  array.each do |ele|
+  
+    new_arr = []
+    array.each do |ele|
     new_ele = ele + ","
     new_arr.push(new_ele)
-  
+    end
   end
-
+  new_arr.insert(-2, "and")
+  return new_arr.join(" ")
 end
